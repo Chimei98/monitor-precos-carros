af64b7d963ce09b0e2336faa2429d4067fdbe3e4
Claudimilson Júnior <seu-email-aqui@exemplo.com>
2026-08-21 11:28:57 +0100

    feat: conectar dados obtidos das variáveis para as colunas da db. IntegrityError previnido.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from asyncio.windows_events import NULL
-
 from bs4 import BeautifulSoup
 import requests
+
+from database import _Sessao, Carro
+from sqlalchemy.exc import IntegrityError
 
 print("Bem vindo ao Sistema de Busca de Carros")
 print("-"*50)
@@ -37,39 +38,60 @@ if resposta.status_code == 200:
     # Padrão - todos os anúncios estão contidos em 'articles' com uma classe geral
     carros = soup.find_all("article", {"class": "ooa-zet1mn e1srzcph1"})
 
-    for carro in carros:
-        nome = carro.find("h2", {"class": "e123dwbo0 ooa-ezpr21"}) # nome do carro
-        preco = carro.find("h3", {"class": "eg88ra81 ooa-3ewd90"}) # preço do carro
-        quilometragem = carro.find("dd", {"data-parameter": "mileage"}) # quilometragem
-        fuel = carro.find("dd", {"data-parameter": "fuel_type"}) # tipo de combustível
-        gearbox = carro.find("dd", {"data-parameter": "gearbox"}) # tipo de caixa
-        link = carro.find("a", {"data-nextlink": "false"})
+    with _Sessao() as sessao:
+        for carro in carros:
+            nome = carro.find("h2", {"class": "e123dwbo0 ooa-ezpr21"}) # nome do carro
+            preco = carro.find("h3", {"class": "eg88ra81 ooa-3ewd90"}) # preço do carro
+            quilometragem = carro.find("dd", {"data-parameter": "mileage"}) # quilometragem
+            fuel = carro.find("dd", {"data-parameter": "fuel_type"}) # tipo de combustível
+            gearbox = carro.find("dd", {"data-parameter": "gearbox"}) # tipo de caixa
+            link = carro.find("a", {"data-nextlink": "false"})
 
-        # Extração de texto para segurança de dados
-        nome_texto = nome.text if nome else "Nome não informado."
-        link_url = link.get("href") if link else "Link não encontrado."
+            # Extração de texto para segurança de dados
+            nome_texto = nome.text if nome else "Nome não informado."
+            link_url = link.get("href") if link else "Link não encontrado."
 
-        # Casting de dados STR => INT de maneira segura.
-        preco_texto = preco.text if preco else "Preço não informado."
-        preco_limpo = 0
-        if preco_texto != "Preço não informado.":
-            preco_limpo = int(preco_texto.replace("EUR", "").replace(" ", "").strip())
+            # Casting de dados STR => INT de maneira segura.
+            preco_texto = preco.text if preco else "Preço não informado."
+            preco_limpo = 0
+            if preco_texto != "Preço não informado.":
+                preco_limpo = int(preco_texto.replace("EUR", "").replace(" ", "").strip())
 
-        quilometragem_texto = quilometragem.text if quilometragem else "Quilometragem não informada."
-        if quilometragem_texto != "Quilometragem não informada.":
-            quilometragem_limpa = int(quilometragem_texto.replace("km", "").replace(" ", "").strip())
+            quilometragem_texto = quilometragem.text if quilometragem else "Quilometragem não informada."
+            quilometragem_limpa = 0
+            if quilometragem_texto != "Quilometragem não informada.":
+                quilometragem_limpa = int(quilometragem_texto.replace("km", "").replace(" ", "").strip())
 
 
-        # Filtro condicional
-        if (goal is not None and preco_limpo > goal) or (goal_km is not None and quilometragem_limpa > goal_km):
-            continue
+            # Filtro condicional
+            if (goal is not None and preco_limpo > goal) or (goal_km is not None and quilometragem_limpa > goal_km):
+                continue
 
-        # Continuação da extração de dados.
-        fuel_texto = fuel.text if fuel else "Combustível não informado."
-        gearbox_texto = gearbox.text if gearbox else "Caixa não informada."
+            # Continuação da extração de dados.
+            fuel_texto = fuel.text if fuel else "Combustível não informado."
+            gearbox_texto = gearbox.text if gearbox else "Caixa não informada."
 
-        print(f"Carro: {nome_texto} | Preço: {preco_texto} | Quilometragem: {quilometragem_texto} | Combustível: {fuel_texto} | Caixa: {gearbox_texto} | Link: {link_url}")
-        print("-"*100)
+            try:
+                sessao.add(Carro(
+                    nome = nome_texto,
+                    preco = preco_limpo,
+                    quilometragem = quilometragem_limpa,
+                    combustivel = fuel_texto,
+                    caixa = gearbox_texto,
+                    link = link_url
+                ))
+                sessao.commit()
+                print(f"SALVO NO BANCO: {nome_texto}")
+                print("-"*50)
+
+            except IntegrityError:
+                # já existe o link
+                sessao.rollback()
+                print(f"Duplicação detectada. IGNORADO: {nome_texto}")
+
+
+            print(f"Carro: {nome_texto} | Preço: {preco_texto} | Quilometragem: {quilometragem_texto} | Combustível: {fuel_texto} | Caixa: {gearbox_texto} | Link: {link_url}")
+            print("-"*100)
 else:
     print("Falha na conexão.")
 
