813d34bc5417b3c53f4f1120e6149693a74ef56d
Claudimilson Júnior <seu-email-aqui@exemplo.com>
2026-08-25 23:34:43 +0100

    feat: implementação do modelo de regressão linear para previsão de preços

--- analise.py
+++ analise.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
@@ -50,15 +51,41 @@ print(fuelgroup_data)
 
 # Transformação de coluna "combustivel" para indicadores binários para análise
 print('-'*100)
-df_binario = pd.get_dummies(df_carros, columns=['combustivel'], dtype=int)
+df_binario = pd.get_dummies(df_carros, columns=['combustivel','caixa'], dtype=int)
 print("DADOS EM BINÁRIO")
 print(df_binario.head())
 
 # Matrizes de isolamento para análise preditiva de preço
 print('-'*100)
-df_x = df_binario.drop(columns=['preco','link']) # variável de entrada
-df_y = df_binario['preco'] # variável pretendida
+df_x = df_binario.drop(columns=['nome','preco','link']) # variável de entrada
+s_y = df_binario['preco'] # variável pretendida
 print("DADOS SUPRIMIDOS")
 print(df_x)
 print('#'*100)
-print(df_y)
+print(s_y)
+
+# Análise Preditiva de preço
+print('-'*100)
+X_treino, X_teste, y_treino, y_teste = train_test_split(df_x, s_y, test_size=0.2, random_state=42) # 80% para treino // 20% para teste
+print("DIMENSÕES DE DIVISÕES DOS DADOS (80/20")
+print("#"*100)
+print(f"Material de estudo (X_treino): {X_treino.shape}") # 6 carros para estudar. Cada um com 6 características (colunas) para estudo => 'quilometragem' e 'combustivel_TIPO'
+print(f"Gabarido (y_treino): {y_treino.shape}") # 6 preços correspondentes => Série
+
+print(f"Prova (X_teste): {X_teste.shape}")
+print(f"Gabarito da prova (y_teste): {y_teste.shape}")
+
+# Criação de modelo matemático para previsão de preços
+modelo = LinearRegression()
+# Treino de modelo
+modelo.fit(X_treino, y_treino)
+# Previsão
+previsoes = modelo.predict(X_teste)
+# Cálculo da margem de erro
+erro = mean_absolute_error(y_teste, previsoes)
+print("-"*100)
+print("Margem de erro de modelo:")
+print(f"    Aproximadamente: {erro} euros") # mais ou menos {erro} euros de diferença.
+
+
+
