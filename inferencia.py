d3538ea5187309eb701b8bcb800bd373d5593865
Claudimilson Júnior <seu-email-aqui@exemplo.com>
2026-08-26 23:20:58 +0100

    feat: incrementar inputs dinamicos para previsao de preco

--- inferencia.py
+++ inferencia.py
@@ -5,16 +5,37 @@ import pandas as pd
 print("[SYS] Iniciando modelo...")
 pilot = joblib.load('modelo_preco.pkl')
 
-# criacao de carro fictício
+dados_quilometros = input("Insira a quantidade de quilometros: ")
+dados_combustivel = input("Insira o tipo de combustivel: ")
+def determinarTipoCombustivel(dados_combustivel):
+    if dados_combustivel == 'diesel':
+        combustivel_Diesel = 1
+        combustivel_Gasolina = 0
+    else:
+        combustivel_Gasolina = 1
+        combustivel_Diesel = 0
+
+    return combustivel_Diesel, combustivel_Gasolina
+
+combustivel_Diesel, combustivel_Gasolina = determinarTipoCombustivel(dados_combustivel)
+
+dados_caixa = input("Insire o tipo de caixa: ")
+def determinarTipoCaixa(dados_caixa):
+    if dados_caixa == 'manual':
+        return 1
+    else:
+        return 0
+
+# criacao de carro com base em dados dinamicos
 dados_novo_carro = {
-    'quilometragem': [150000],
-    'combustivel_Diesel': [1],
-    'combustivel_Gasolina': [0],
-    'caixa_Manual': [1]
+    'quilometragem': dados_quilometros,
+    'combustivel_Diesel': combustivel_Diesel,
+    'combustivel_Gasolina': combustivel_Gasolina,
+    'caixa_Manual': determinarTipoCaixa(dados_caixa)
 }
 
 # DataFrame para leitura pelo modelo
-df_novo_carro = pd.DataFrame(dados_novo_carro)
+df_novo_carro = pd.DataFrame([dados_novo_carro])
 
 print("[SYS] Carro fictício inicializado...")
 print(df_novo_carro)
